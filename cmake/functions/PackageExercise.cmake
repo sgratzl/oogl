0607b156a31ff0f1bc25e72844b9babc514f7312
Samuel Gratzl <samuel_gratzl@gmx.at>
2011-02-17 10:26:55 +0000

    integrate ANT into CMAKE using custom target

---
+++ cmake/functions/PackageExercise.cmake
@@ -0,0 +1,19 @@
+function(package_exercise)
+	if(NOT INTERNAL_AVALIABLE)
+		return()
+	endif()
+
+	find_package(ANT)
+
+	if(ANT_FOUND)
+		message(STATUS "adding package target for ${CURRENT_NAME}")
+		get_filename_component(CURRENT_DIR "${CMAKE_CURRENT_SOURCE_DIR}" NAME)
+		add_custom_target(package${CURRENT_NAME}
+			"${ANT_EXE}" "-Dexercise.name=${CURRENT_DIR}" "-buildfile=assembly.xml"
+			WORKING_DIRECTORY "${INTERNAL_DIR}"
+			COMMENT "running ANT" VERBATIM)
+	else()
+		message(WARNING "can't find ANT check your settings otherwise you won't be able to build a package")
+	endif()
+endfunction(package_exercise)
+
