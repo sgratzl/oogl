d9b340d976d1cdebed83cc92ee9d60e9d78604b2
Samuel Gratzl <samuel_gratzl@gmx.at>
2011-03-04 21:34:34 +0000

    try to fix unix errors

--- exercises/common/oogl/model/Model3ds.cpp
+++ exercises/common/oogl/model/Model3ds.cpp
@@ -25,6 +25,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
 #include <utils/log.h>
 
 #include <cstring>
+#include <stdexcept>
 
 #include <glm/glm_ostream.hpp>
 #include <glm/gtc/type_ptr.hpp>
